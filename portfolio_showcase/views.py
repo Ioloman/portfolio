173ffc205598fa9da7119b626995022d49eeb56d
loloman <kirilldd2@gmail.com>
2020-06-21 19:45:53 +0700

    added model to admin; added homepage template with some django logic

--- portfolio_showcase/views.py
+++ portfolio_showcase/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Portfolio_DB
 
 
 def homepage(request):
-    return render(request, '../templates/portfolio_showcase/home.html')
+    objects = Portfolio_DB.objects.all()
+    return render(request, '../templates/portfolio_showcase/home.html', {'portfolios': objects})
