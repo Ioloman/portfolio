1f06954edb001fcb52fcdf2409368c03ef9103a2
loloman <kirilldd2@gmail.com>
2020-06-21 23:36:41 +0700

    added blog template; added static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+def blog_main(request):
+    articles = Article.objects.order_by('-date')[:5]
+    return render(request, '../templates/blog/blog_main.html', {'articles': articles})
