dd09da98a94c2f30b7c2af770539673f7ca08c14
loloman <kirilldd2@gmail.com>
2020-06-23 00:45:56 +0700

    localization

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,9 @@ from .models import Article
 
 def blog_main(request):
     articles = Article.objects.order_by('-date')
-    return render(request, '../templates/blog/blog_main.html', {'articles': articles, 'title': 'Блог'})
+    return render(request, '../templates/blog/blog_main.html', {'articles': articles, 'title': 'Blog'})
 
 
 def blog_detail(request, blog_id):
     blog = get_object_or_404(Article, pk=blog_id)
-    return render(request, '../templates/blog/blog_detail.html', {'blog': blog, 'title': 'Статья - ' + str(blog.title)})
+    return render(request, '../templates/blog/blog_detail.html', {'blog': blog, 'title': 'Article - ' + str(blog.title)})
